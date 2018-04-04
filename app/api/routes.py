42d127763e1d2f6201d36b8afd030320de544062
Henrik Böving <hargonix@gmail.com>
2018-04-04 22:10:02 +0200

    testing something

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,8 +1,14 @@
 from app.api import bp
 from flask import request, jsonify
 
+
 @bp.route("/dust_api", methods=["POST"])
 def dust_api():
     sensor_values = request.get_json()["sensordatavalues"]
     print(sensor_values)
     return ""
+
+
+@bp.route("/dust_api", methods=["GET"])
+def dust_api_get():
+    return "dust_api"
